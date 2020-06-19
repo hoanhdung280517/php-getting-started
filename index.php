870f28854a84843ccbccef72206064eb27298e49
hoanhdung280517 <66712810+hoanhdung280517@users.noreply.github.com>
2020-06-19 14:30:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+	include 'inc/header.php';
+	include 'inc/slider.php';
+?>
+ <div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="heading">
+    		<h3>Hot Toys</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+	      <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php"><img src="images/5588-606_1.jpg" alt="" /></a>
+					 <h2>Future supercar 2080 </h2>
+					 <p>Control cars coming from the future 2050 use rechargeable batteries with batteries available in the box, know how to turn around 360 degrees</p>
+					 <p><span class="price">349,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/34195-1.jpg" alt="" /></a>
+					 <h2>ELECTRONIC KEYBOARD </h2>
+					 <p>Electronic keyboard includes 7 exciting soundtrack songs. There is a recording and playback function. He freely creates many melodies in his own way</p>
+					 <p><span class="price">587,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/6046156_1.png" alt="" /></a>
+					 <h2>Owleez Owl learn to fly</h2>
+					 <p>Owleez is a new generation interactive toy capable of flying for the first time. You will teach Owleez to learn how to fly by interacting with it as much as possible.</p>
+					 <p><span class="price">803,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<img src="images/6046466-4.jpg" alt="" />
+					 <h2>Construction vehicle Paw Patrol</h2>
+					 <p>Toy brand from Canada, modeled after the world famous cartoon Paw Patrol Rescue Dog Team. Product dimensions: 40.6 x 17.8 x 33 (cm).</p>
+					 <p><span class="price">951,000 VNĐ</span></p>  
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>New Toys</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php"><img src="images/e6943_2.jpg" alt="" /></a>
+					 <h2>Little Cherry lady</h2>
+					 <p>Cherry is a line of dolls to eat and drink water to pee Baby Alive to make the moments make her memorable. These will be interesting experiences about being brothers and sisters.
+					 </p>
+					 <p><span class="price">766,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/eu730809_2_1.jpg" alt="" /></a>
+					 <h2>Giant MIRA Rescue Submarine </h2>
+					 <p>Not only can the rescue ship walk on the ground, it can move the water. Press the button, a net is launched, and the child can rescue many underwater creatures and hand out lost items.
+					 </p>
+					 <p><span class="price">657,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/r74620_yel_2.jpg" alt="" /></a>
+					 <h2>Driver car transforms Pagani</h2>
+					 <p>With the speed of 8km / h, the control distance is up to 30 meters, the suspension system, removable rubber tires. Rastar deserves to be the leading control vehicle manufacturer</p>
+					 <p><span class="price">$428.02</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/r75100_ora_2.jpg" alt="" />
+					 <h2>Mc Laren P1 </h2>
+					 <p>With a speed of 8km / h, a driving distance of 30 meters, shock absorbers, and fully removable rubber tires, Rastar deserves to be the leading manufacturer of control vehicles for children.</p>				 
+					 <p><span class="price">$457.88</span></p>
+
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+    </div>
+ </div>
+ 
+ <?php
+	include 'inc/footer.php';
+?>
---
+++ products.php
@@ -0,0 +1,239 @@
+<?php
+	include 'inc/header.php';
+	include 'inc/slider.php';
+?>
+ <div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="heading">
+    		<h3>Car Control</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+	      <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/car1.jpg" alt="" /></a>
+					 <h2>Bugatti Chiron </h2>
+					 <p><span class="price">379,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/car2.jpg" alt="" /></a>
+					 <h2>Ferrari 458 Speciale A</h2>
+					 <p><span class="price">649,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/car3.jpg" alt="" /></a>
+					 <h2>Audi R8 new version</h2>
+					 <p><span class="price">449,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<img src="images/R72100_R72100-2_1.jpg" alt="" />
+					 <h2>Mercedes-benz</h2>
+					 <p><span class="price">665,552 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+	      <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/R71900-YEL_1.jpg" alt="" /></a>
+					 <h2>R / C 1:18 BMW i8 cars</h2>
+					 <p><span class="price">659,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/R53900_1.jpg" alt="" /></a>
+					 <h2>Bugatti Veyron Grand Sport</h2>
+					 <p><span class="price">659,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/r53800_1.jpg" alt="" /></a>
+					 <h2>Ferrari F1 car</h2>
+					 <p><span class="price">649,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<img src="images/46300-orange_1.jpg" alt="" />
+					 <h2>Aventador LP700 car</h2>
+					 <p><span class="price">379,000 VNĐ</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+
+
+
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Spindle</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/2260_1.jpg" alt="" /></a>
+					 <h2>Icy Dragon Spinning Attack</h2>
+					 <p><span class="price">400,000 VNĐ</span></p>
+				    
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/2256_1.jpg" alt="" /></a>
+					 <h2>Lord Of Garmadon</h2>
+					 <p><span class="price">125,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/2255_1.jpg" alt="" /></a>
+					 <h2>Master Wu's Ferris Wheel</h2>
+					 <p><span class="price">125,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/2171_1.jpg" alt="" />
+					 <h2>Zane Spinning</h2>					 
+					 <p><span class="price">125,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+
+
+
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Underwater toys</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/BT2433_1.jpg" alt="" /></a>
+					 <h2>Marine life fishing suit</h2>
+					 <p><span class="price">539,000 VNĐ</span></p>
+				    
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/000501_1.jpg" alt="" /></a>
+					 <h2>Go to the beach with baby turtles</h2>
+					 <p><span class="price">239,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/000616_1.jpg" alt="" /></a>
+					 <h2>Marine tools</h2>
+					 <p><span class="price">89,000 VNĐĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/11202_1_11202.gif" alt="" />
+					 <h2>Toy Tomy Bear Puddle</h2>					 
+					 <p><span class="price">105,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+
+
+
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Vali</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/16326-6045-00_01.jpg" alt="" /></a>
+					 <h2>360-degree rotating suitcase Paw Patrol Code PL23 - 18 inches</h2>
+					 <p><span class="price">1,049,000 VNĐ</span></p>
+				    
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/16325-6045-00_01.jpg" alt="" /></a>
+					 <h2>360-degree rotating suitcase Paw Patrol Code PL22 - 18 inches</h2>
+					 <p><span class="price">1,049,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/h-hsrl-sp-mp02-16fa_01.jpg" alt="" /></a>
+					 <h2>360-degree rotating suitcase Little Pony Code MP02 - 18 inches</h2>
+					 <p><span class="price">1,049,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/16321-6045-00_1.jpg" alt="" />
+					 <h2>Swing suitcase 360 Figure Paw Patrol Blue red - 18 inches</h2>					 
+					 <p><span class="price">1,049,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+
+
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>Brain teaser</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/5887640005_1.jpg" alt="" /></a>
+					 <h2>Mini 64 color brush (washable)</h2>
+					 <p><span class="price">419,000 VNĐ</span></p>
+				    
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/5887090001_1.jpg" alt="" /></a>
+					 <h2>Mini pen with 16 colors</h2>
+					 <p><span class="price">195,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/588703_1.jpg" alt="" /></a>
+					 <h2>Mini brush set with 16 colors</h2>
+					 <p><span class="price">189,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/588626_1.jpg" alt="" />
+					 <h2>Brush draw on fabric</h2>					 
+					 <p><span class="price">72,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details-3.php"><img src="images/587812_1.jpg" alt="" /></a>
+					 <h2>12 color thick brush (washable)</h2>
+					 <p><span class="price">179,000 VNĐ</span></p>
+				    
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-4.php"><img src="images/587808_1.jpg" alt="" /></a>
+					 <h2>8-color thick brush (washable)</h2>
+					 <p><span class="price">33,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details-2.php"><img src="images/5877260016_1.jpg" alt="" /></a>
+					 <h2>Fine 10-color brush strokes</h2>
+					 <p><span class="price">27,000 VNĐ</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/5462010001_1.jpg" alt="" />
+					 <h2>5-color brush set</h2>					 
+					 <p><span class="price">54,000 VNĐ</span></p>   
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+    </div>
+ </div>
+<?php
+	include 'inc/footer.php';
+?>
