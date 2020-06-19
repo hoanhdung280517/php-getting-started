bd1edd80e358b470d5a6f65e03e406144070e8d8
hoanhdung280517 <66712810+hoanhdung280517@users.noreply.github.com>
2020-06-19 16:10:10 +0700

    web

---
+++ inc/slider.php
@@ -0,0 +1,66 @@
+<div class="header_bottom">
+		<div class="header_bottom_left">
+			<div class="section group">
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_2_of_1">
+						 <a href="preview.php"> <img src="images/vn858320-r_1.jpg" alt="" /></a>
+					</div>
+				    <div class="text list_2_of_1">
+						<h2>Control car</h2>
+						<p>The space monster's remote control car has a unique design, can be rolled up and rolled over an edge.</p>
+						<div class="button"><span><a href="preview.php">Add to cart</a></span></div>
+				   </div>
+			   </div>			
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_2_of_1">
+						  <a href="preview.php"><img src="images/6055944_1.png" alt="" / ></a>
+					</div>
+					<div class="text list_2_of_1">
+						  <h2>Batman and armed</h2>
+						  <p>Batman have 3 flexible joints and a cloth jacket with sharp details .Armed with a waistband with 3 power weapons.</p>
+						  <div class="button"><span><a href="preview.php">Add to cart</a></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="section group">
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_2_of_1">
+						 <a href="preview.php"> <img src="images/1_1.jpg" alt="" /></a>
+					</div>
+				    <div class="text list_2_of_1">
+						<h2>Swarovski bracelet</h2>
+						<p>A manufacturer of toys for girls Make It Real,brand from the UK.Baby can make 7 bracelets</p>
+						<div class="button"><span><a href="preview.php">Add to cart</a></span></div>
+				   </div>
+			   </div>			
+				<div class="listview_1_of_2 images_1_of_2">
+					<div class="listimg listimg_2_of_1">
+						  <a href="preview.php"><img src="images/e1952_1.jpg" alt="" /></a>
+					</div>
+					<div class="text list_2_of_1">
+						  <h2>Archaeo
+						  logist</h2>
+						  <p>Products include:1 dinosaur,4 boxes of Playdoh, 1 shaped frame,1 lava,1 knife.</p>
+						  <div class="button"><span><a href="preview.php">Add to cart</a></span></div>
+					</div>
+				</div>
+			</div>
+		  <div class="clear"></div>
+		</div>
+			 <div class="header_bottom_right_images">
+		   <!-- FlexSlider -->
+             	
+			<section class="slider">
+				  <div class="flexslider">
+					<ul class="slides">
+						<li><img src="images/01.jpeg" alt=""/></li>
+						<li><img src="images/02.jpg" alt=""/></li>
+						<li><img src="images/03.png" alt=""/></li>
+						<li><img src="images/04.png" alt=""/></li>
+				    </ul>
+				  </div>
+	      </section>
+<!-- FlexSlider -->
+	    </div>
+	  <div class="clear"></div>
+  </div>	
