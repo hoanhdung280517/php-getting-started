c1ef7c1467cd28440bca79b3c0c348afeee41227
hoanhdung280517 <66712810+hoanhdung280517@users.noreply.github.com>
2020-06-24 11:42:26 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "www.db4free.net");
 define("DB_USER", "dung_shopdochoi");
-define("DB_PASS", "Dung280517")
+define("DB_PASS", "Dung280517");
 define("DB_NAME", "shopdochoitrecon");
 ?>
