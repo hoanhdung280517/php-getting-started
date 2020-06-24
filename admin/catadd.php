a42c3ecb23c473173a305f992853e0be2ca3cd07
hoanhdung280517 <66712810+hoanhdung280517@users.noreply.github.com>
2020-06-24 11:03:19 +0700

    Update catadd.php

--- admin/catadd.php
+++ admin/catadd.php
@@ -1,31 +1,30 @@
 <?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
-<?php include '../classes/category.php'?> 
+<?php include '../classes/product.php'?>
 <?php
-    $cat = new category();
+    $cat = new product();
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $catName = $_POST['catName'];
-        $insertCat = $cat->insert_category($catName);
+    
+        $insertCat = $cat->insert_product($catName);
     }
 ?>
-<?php ?> 
         <div class="grid_10">
             <div class="box round first grid">
-                <h2>Add New Category</h2>  
+                <h2>Add New Product</h2>
                <div class="block copyblock"> 
-                 <?php
-                    if(isset($insertCat)){
-                        echo $insertCat;
-                    }
-                 ?>
-                 <form action="catadd.php" method="post">
-                    <table class="form">					
+               <?php if (isset($insertCat)){
+                          echo $insertCat;
+                      }
+                   ?>
+                 <form action ="catadd.php" method = "post">
+                    <table class="form">                    
                         <tr>
                             <td>
-                                <input type="text" name="catName" placeholder="Enter Category Name..." class="medium" />
+                                <input type="text" name="catName" placeholder="Enter Product Name..." class="medium" />
                             </td>
                         </tr>
-						<tr> 
+                        <tr> 
                             <td>
                                 <input type="submit" name="submit" Value="Save" />
                             </td>
